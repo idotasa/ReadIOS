ca44e50a43e10e576cfd3cb423b6a6369780dfec
ofir kaya <kayaofir@gmail.com>
2025-07-15 01:03:18 +0300

    front backend connection

--- readios-server/server.js
+++ readios-server/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../readios-client')));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
