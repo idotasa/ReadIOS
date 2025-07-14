e49f27beece08d5d2cad07942c1036abea6e8f5e
Ido Tasa <idotasa5@gmail.com>
2025-07-15 00:26:37 +0300

    add first models of User, Group and Post

---
+++ readios-server/models/Post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  userId:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content:   { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+}, { _id: true });
+
+const postSchema = new mongoose.Schema({
+  userId:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  groupId:  { type: mongoose.Schema.Types.ObjectId, ref: 'Group' }, // אופציונלי
+  title:    { type: String, required: true },
+  content:  { type: String },
+  type:     { type: String, enum: ['text', 'image', 'video', 'image+text'], required: true },
+  url:      { type: String }, // Optional URL for image or video
+  likes:    [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [commentSchema],
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Post', postSchema);
