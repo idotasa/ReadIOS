ad77192ccb594d0502fe6d446cb476943d1d96c6
ofir kaya <kayaofir@gmail.com>
2025-07-19 18:25:41 +0300

    add file

---
+++ readios-server/routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser, getUserById, updateUserDetails, deleteUser, addFriend, removeFriend, searchUsersContainsName } = require('../controllers/userController');
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/', searchUsersContainsName);
+router.get('/:id', getUserById);
+router.put('/:id', updateUserDetails);
+router.delete('/:id', deleteUser);
+
+router.post('/:id/friend/:friendId', addFriend);
+router.delete('/:id/friend/:friendId', removeFriend);
+
+module.exports = router;
