53bc31910553e4df988a75a6146a484f3a08f701
ofir kaya <kayaofir@gmail.com>
2025-07-19 18:22:39 +0300

    delete photos

--- readios-server/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser, getUserById } = require('../controllers/userController');
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/:id', getUserById);
-
-module.exports = router;
