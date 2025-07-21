50892168a6156b1b5ee8055c37fb4808f2c2712a
Ido Tasa <idotasa5@gmail.com>
2025-07-21 21:52:07 +0300

    add auth.js and validation for some routes

--- readios-server/routes/posts.js
+++ readios-server/routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { createPost, deletePost, addComment, toggleLike, getPostById, searchPosts } = require('../controllers/postController');
+const verifyToken = require('../middleware/auth');
 
-router.post('/', createPost);
-router.delete('/:id', deletePost);
-router.post('/:id/comments', addComment);
-router.put('/:id/like', toggleLike);
-router.get('/:id', getPostById);
-router.get('/', searchPosts);
+router.post('/', verifyToken, createPost);
+router.delete('/:id', verifyToken, deletePost);
+router.post('/:id/comments', verifyToken, addComment);
+router.put('/:id/like', verifyToken, toggleLike);
+router.get('/:id', verifyToken, getPostById);
+router.get('/', verifyToken, searchPosts);
 
 module.exports = router;
--- readios-server/routes/users.js
+++ readios-server/routes/users.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getUserById, updateUserDetails, deleteUser, addFriend, removeFriend, searchUsersContainsName } = require('../controllers/userController');
+const verifyToken = require('../middleware/auth');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/', searchUsersContainsName);
-router.get('/:id', getUserById);
-router.put('/:id', updateUserDetails);
-router.delete('/:id', deleteUser);
+router.get('/', verifyToken, searchUsersContainsName);
+router.get('/:id', verifyToken, getUserById);
+router.put('/:id', verifyToken, updateUserDetails);
+router.delete('/:id', verifyToken, deleteUser);
 
-router.post('/:id/friend/:friendId', addFriend);
-router.delete('/:id/friend/:friendId', removeFriend);
+router.post('/:id/friend/:friendId', verifyToken, addFriend);
+router.delete('/:id/friend/:friendId', verifyToken, removeFriend);
 
 module.exports = router;
